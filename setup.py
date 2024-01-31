c76ec4c7ffe6f090ba7b37ac9c16e399927aebea
Sebastian Alvarez <ca.sebastianlv@gmail.com>
2024-01-30 23:35:45 -0300

    Constructor del paquete

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tu-ruc-python-client',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests'
+    ],
+    author='Sebastian Alvarez',
+    author_email='ca.sebastianlv@gmail.com',
+    description='Cliente Python para la API de TURUC Paraguay ',
+    url='https://github.com/seb5433/tu-ruc-python-client',
+)
